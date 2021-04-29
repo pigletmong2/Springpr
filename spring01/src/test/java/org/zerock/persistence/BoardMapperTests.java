4e0f53e5d91c761bb0eaafe4bf92fa5ec3c55744
pigletmong2 <doheekm@naver.com>
2021-04-29 12:08:09 +0900

    Spring Web Project Test

---
+++ spring01/src/test/java/org/zerock/persistence/BoardMapperTests.java
@@ -0,0 +1,24 @@
+package org.zerock.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	@Setter(onMethod_= {@Autowired})
+	private BoardMapper mapper;
+	
+	@Test
+	public void testGetList() { /* 작성한 mapper interface테스트 */
+		mapper.getList().forEach(board ->log.info(board));
+	}
+}
